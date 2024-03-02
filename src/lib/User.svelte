2b31c9b8c31c8543fbcf5d9a56a650f951297153
Alexandre Flion <huntears@kreog.com>
2024-03-02 17:08:15 +0100

    Format code using prettier
    Pretty sure I did it before but looks like I forgot :3
    
    Signed-off-by: Alexandre Flion <huntears@kreog.com>

--- src/lib/User.svelte
+++ src/lib/User.svelte
@@ -6,11 +6,11 @@
 </script>
 
 <li>
-	<span class="text-xl font-mono">
+	<span class="font-mono text-xl">
 		{username}
 	</span>
 	{#if Object.keys(info.invited).length !== 0}
-		<ul class="pl-3 ml-1 border-l border-l-slate-400 dark:border-l-slate-600">
+		<ul class="ml-1 border-l border-l-slate-400 pl-3 dark:border-l-slate-600">
 			{#each Object.entries(info.invited) as [username, new_info]}
 				<svelte:self {username} info={new_info} />
 			{/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,12 @@
 	import raw_tree from './users.json';
 	import User from '$lib/User.svelte';
 
-	const tree = raw_tree as UserInfo["invited"];
-
+	const tree = raw_tree as UserInfo['invited'];
 </script>
 
-<main class="absolute inset-0 flex items-center flex-col p-8">
+<main class="absolute inset-0 flex flex-col items-center p-8">
 	<h1 class="text-5xl font-bold">KreogMC</h1>
-	<ul class="grow flex items-center">
+	<ul class="flex grow items-center">
 		<User username="huntears" info={tree.huntears} />
 	</ul>
 </main>
