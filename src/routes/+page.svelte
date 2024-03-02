2c8a1c543016bbc8d6737f3ccdac5ce6dea57480
Alexandre Flion <huntears@kreog.com>
2024-03-02 10:44:20 +0100

    Add a title to the main page
    Signed-off-by: Alexandre Flion <huntears@kreog.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,9 @@
 
 </script>
 
-<main class="absolute inset-0 flex items-center justify-center">
-	<ul>
+<main class="absolute inset-0 flex items-center flex-col p-8">
+	<h1 class="text-5xl font-bold">KreogMC</h1>
+	<ul class="grow flex items-center">
 		<User username="huntears" info={tree.huntears} />
 	</ul>
 </main>
